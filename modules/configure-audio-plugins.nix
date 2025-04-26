90c638af0e672ad08ad4e86abed682cd74718a9a
ginkogruen <jasper@wolter-koeln.de>
2025-04-26 13:19:12 +0200

    --amend

--- modules/configure-audio-plugins.nix
+++ modules/configure-audio-plugins.nix
@@ -1,10 +1,12 @@
 {
+  inputs,
   config,
   lib,
   ...
 }: let
   inherit (lib) mkEnableOption mkIf;
   cfg = config.nix-audio-plugins;
+  self = inputs.nix-audio-plugins;
 in {
   options.nix-audio-plugins = {
     enable = mkEnableOption "Enable Zebralette3 plugin linking";
@@ -15,15 +17,15 @@ in {
       echo "Linking Zebralette3 to plugin paths..."
 
       mkdir -p ~/.vst3/u-he
-      cp -r ${/*self.packages."x86_64-linux".zebralette3-beta*/}/lib/vst3/u-he/Zebralette3.vst3 ~/.vst3/u-he/Zebralette3.vst3
+      cp -r ${self.packages."x86_64-linux".zebralette3-beta}/lib/vst3/u-he/Zebralette3.vst3 ~/.vst3/u-he/Zebralette3.vst3
       chmod -R 775 ~/.vst3/u-he/Zebralette3.vst3
 
       mkdir -p ~/.clap/u-he
-      cp -r ${/*self.packages."x86_64-linux".zebralette3-beta*/}/lib/clap/u-he/Zebralette3.clap ~/.clap/u-he/Zebralette3.clap
+      cp -r ${self.packages."x86_64-linux".zebralette3-beta}/lib/clap/u-he/Zebralette3.clap ~/.clap/u-he/Zebralette3.clap
       chmod -R 775 ~/.clap/u-he/Zebralette3.clap
 
       mkdir -p ~/.u-he
-      cp -r ${/*self.packages."x86_64-linux".zebralette3-beta*/}/share/Zebralette3 ~/.u-he/Zebralette3
+      cp -r ${self.packages."x86_64-linux".zebralette3-beta}/share/Zebralette3 ~/.u-he/Zebralette3
       chmod -R 775 ~/.u-he
     '';
   };
